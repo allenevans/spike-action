1e02170e812ab0a03acebf7a58b042f577766362
Allen Evans <allen@techinity.com>
2019-09-13 06:22:09 +0100

    test image

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,11 @@ ls
 cat README.md
 
 echo "hello $1" >> README.md
+
+printenv
+
+echo "node version: $(node -v)"
+echo "lerna version: $(lerna -v)"
+echo "js version: $(jq --version)"
+
+lerna changed --json --loglevel=silent | jq -c -r 'map(.name) | if (. | length) == 1 then join("") else join(",") | "{" + . + "}" end'
