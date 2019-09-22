4d4d44c1c4abe3298f0d397cd354025f9c93e708
Allen Evans <allen@techinity.com>
2019-09-22 08:25:14 +0100

    remove undefined commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh -l
 
 pwd
-
-ls
-
-cat README.md
-
-echo "hello $1" >> README.md
-
 printenv
-
-echo "node version: $(node -v)"
-echo "lerna version: $(lerna -v)"
-echo "js version: $(jq --version)"
-
-lerna changed --json --loglevel=silent | jq -c -r 'map(.name) | if (. | length) == 1 then join("") else join(",") | "{" + . + "}" end'
